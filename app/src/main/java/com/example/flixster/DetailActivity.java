bcd173b52ba9282377a3234d87acebb377bdca75
georgecar4 <georgecar4@gmail.com>
2021-07-16 23:12:24 -0500

    code without  the API key

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -23,7 +23,7 @@ import models.Movie;
 import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
-    // githut sent an email saying I should not share my API KEY that why it is not there
+    // githut sent an email saying I should not share my API KEY that why it is not there in the quotes
     public  static final String YOUTUBE_API_KEY = "";
     public  static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     TextView tvTitle;
